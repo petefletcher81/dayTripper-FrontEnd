ceb0f72d562af4550b4e5afd1f4ca586e1a2f0ac
RachGriff <rlgriffiths@hotmail.co.uk>
2018-11-23 13:04:14 +0000

    get new rootStack working

---
+++ components/JourneyPlanner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default class JourneyPlannerScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Journey Planner Screen</Text>
+      </View>
+    );
+  }
+}
