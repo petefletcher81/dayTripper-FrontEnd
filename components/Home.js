67472ff0fdca3fdba291848557ea727d36aa9b90
RachGriff <rlgriffiths@hotmail.co.uk>
2018-11-23 12:38:48 +0000

    pages linking

--- components/Home.js
+++ components/Home.js
@@ -1,23 +1,47 @@
 // In App.js in a new project
 
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
-
+import LoginScreen from "./login";
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Home Screen</Text>
+        <Button
+          title="Login"
+          onPress={() => {
+            this.props.navigation.navigate("Login");
+          }}
+        />
       </View>
     );
   }
 }
+// class LoginScreen extends React.Component {
+//   render() {
+//     return (
+//       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//         <Text>Login Screen</Text>
+//       </View>
+//     );
+//   }
+// }
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Login: LoginScreen
+  },
+  {
+    initialRootName: "Home"
   }
-});
+);
+// const AppNavigator = createStackNavigator({
+//   Home: {
+//     screen: HomeScreen
+//   }
+// });
 
-export default createAppContainer(AppNavigator);
+export default createAppContainer(RootStack);
