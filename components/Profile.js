440e6f05ff78eb7c180650d1025647fe1b4e98b9
Pete Fletcher <petefletcher@mbp0012-2.local>
2018-11-28 16:09:19 +0000

    intial add user completed

--- components/Profile.js
+++ components/Profile.js
@@ -1,17 +1,71 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Picker } from "react-native";
 // import DashBoard from './DashBoard'
+import {
+  FormLabel,
+  FormInput,
+  FormValidationMessage,
+  CheckBox
+} from "react-native-elements";
 
 export default class ProfileScreen extends React.Component {
+  state = {
+    username: "",
+    firstname: "",
+    lastname: "",
+    preference1: false,
+    checked: false
+  };
+
+  handleSubmit = () => {
+    onChangeText = "";
+    fetch(
+      "https://xprfmsf0pb.execute-api.eu-west-1.amazonaws.com/dev/createUser",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: this.state.username,
+          firstname: this.state.firstname,
+          lastname: this.state.lastname
+        })
+      }
+    ).then(res => {
+      console.log(res);
+      this.setState({
+        username: "",
+        firstname: "",
+        lastname: ""
+      });
+    });
+  };
+
   render() {
+    console.log(this.state.firstname);
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Profile Screen</Text>
-        {/* <Button title='send to dashboard'onPress={()=>{
-          this.props.navigation.navigate("DashBoard")
-        }} */}
-        {/* /> */}
+        <FormLabel>username</FormLabel>
+        <FormInput
+          value={this.state.username}
+          onChangeText={username => this.setState({ username })}
+        />
+        <FormLabel>Firstname</FormLabel>
+        <FormInput
+          value={this.state.firstname}
+          onChangeText={firstname => this.setState({ firstname })}
+        />
+        <FormLabel>Surname</FormLabel>
+        <FormInput
+          value={this.state.lastname}
+          onChangeText={lastname => this.setState({ lastname })}
+        />
+        <Button title="Save" onPress={this.handleSubmit} />
       </View>
     );
   }
+
+  componentDidMount = () => {};
 }
