98ab2188b48832dd7caadf6a03b1a2d9acba3195
Pete Fletcher <petefletcher@mbp0012-2.local>
2018-11-28 12:03:25 +0000

    datepick and plan reworked

--- components/DatePicker.js
+++ components/DatePicker.js
@@ -1,34 +1,32 @@
 import React, { Component } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import DateTimePicker from "react-native-modal-datetime-picker";
-
 export default class DateTimePickerTester extends Component {
   state = {
     isDateTimePickerVisible: false,
-    date: ""
+    date: null
   };
-
   _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
-
   _hideDateTimePicker = date => {
     // console.log(date);
     this.setState({
       isDateTimePickerVisible: false,
-      date: date.toString().slice(0, -23)
+      date: date
     });
   };
   _handleDatePicked = date => {
     // console.log("A date has been picked: ", date);
     this._hideDateTimePicker(date);
   };
-
   render() {
     console.log(this.state);
     return (
       <View style={{ flex: 1 }}>
         <TouchableOpacity onPress={this._showDateTimePicker}>
           <Text>Show DatePicker</Text>
-          <Text>{this.state.date}</Text>
+          {this.state.date && (
+            <Text>{this.state.date.toString().slice(0, -23)}</Text>
+          )}
         </TouchableOpacity>
         <DateTimePicker
           isVisible={this.state.isDateTimePickerVisible}
