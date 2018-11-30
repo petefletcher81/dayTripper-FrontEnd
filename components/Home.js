d503a171ebea23df7042382504d95ea2f8c913e6
jugelington <julianbeightondykes@gmail.com>
2018-11-30 12:36:19 +0000

    created drawer

--- components/Home.js
+++ components/Home.js
@@ -1,46 +1,62 @@
 // In App.js in a new project
 
-import React from "react";
-import { View, Text, Button } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
-import LoginScreen from "./Login";
-import SignUpScreen from "./SignUp";
-import JourneyPlannerScreen from "./JourneyPlanner.js";
-import DashBoardScreen from "./DashBoard";
-import PlanScreen from "./Plan";
-import ProfileScreen from "./Profile";
-import HistoryScreen from "./History";
-import SuggestionsScreen from "./Suggestions";
-import ItineraryScreen from "./Itinerary";
-import MapScreen from "./Map.js";
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import {
+  createDrawerNavigator,
+  createStackNavigator,
+  createAppContainer,
+  DrawerActions,
+} from 'react-navigation';
+import { Icon, Header, Image } from 'react-native-elements';
+import LoginScreen from './Login';
+import SignUpScreen from './SignUp';
+import JourneyPlannerScreen from './JourneyPlanner.js';
+import DashBoardScreen from './DashBoard';
+import PlanScreen from './Plan';
+import ProfileScreen from './Profile';
+import HistoryScreen from './History';
+import SuggestionsScreen from './Suggestions';
+import ItineraryScreen from './Itinerary';
+import MapScreen from './Map.js';
 
 class HomeScreen extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Home',
+  };
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {/* <Nav /> */}
         <Text>Home Screen</Text>
+        <Button
+          title="openMenu"
+          onPress={() => this.props.navigation.openDrawer()}
+        >
+          Open Menu
+        </Button>
         <Button
           title="Login"
           onPress={() => {
-            this.props.navigation.navigate("Login");
+            this.props.navigation.navigate('Login');
           }}
         />
         <Button
           title="Sign up"
           onPress={() => {
-            this.props.navigation.navigate("SignUp");
+            this.props.navigation.navigate('SignUp');
           }}
         />
         <Button
           title="Itinerary"
           onPress={() => {
-            this.props.navigation.navigate("Itinerary");
+            this.props.navigation.navigate('Itinerary');
           }}
         />
         <Button
           title="Skip"
           onPress={() => {
-            this.props.navigation.navigate("Suggestions");
+            this.props.navigation.navigate('Suggestions');
           }}
         />
       </View>
@@ -48,23 +64,82 @@ class HomeScreen extends React.Component {
   }
 }
 
-const RootStack = createStackNavigator(
+// const MyDrawerNavigator = createDrawerNavigator({
+//   Home: { screen: HomeScreen },
+// });
+
+const RootStack = createDrawerNavigator(
   {
-    Home: HomeScreen,
-    Login: LoginScreen,
-    SignUp: SignUpScreen,
-    JourneyPlanner: JourneyPlannerScreen,
-    DashBoard: DashBoardScreen,
-    Plan: PlanScreen,
-    Profile: ProfileScreen,
-    History: HistoryScreen,
-    Suggestions: SuggestionsScreen,
-    Itinerary: ItineraryScreen,
-    Map: MapScreen
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: () => ({
+        title: 'Home',
+      }),
+    },
+    Login: {
+      screen: LoginScreen,
+      navigationOptions: () => ({
+        title: 'Login',
+      }),
+    },
+    SignUp: {
+      screen: SignUpScreen,
+      navigationOptions: () => ({
+        title: 'SignUp',
+      }),
+    },
+    JourneyPlanner: {
+      screen: JourneyPlannerScreen,
+      navigationOptions: () => ({
+        title: 'Journey Planner',
+      }),
+    },
+    DashBoard: {
+      screen: DashBoardScreen,
+      navigationOptions: () => ({
+        title: 'Dashboard',
+      }),
+    },
+    Plan: {
+      screen: PlanScreen,
+      navigationOptions: () => ({
+        title: 'Plan',
+      }),
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: () => ({
+        title: 'Profile',
+      }),
+    },
+    History: {
+      screen: HistoryScreen,
+      navigationOptions: () => ({
+        title: 'History',
+      }),
+    },
+    Suggestions: {
+      screen: SuggestionsScreen,
+      navigationOptions: () => ({
+        title: 'Suggestions',
+      }),
+    },
+    Itinerary: {
+      screen: ItineraryScreen,
+      navigationOptions: () => ({
+        title: 'Itinerary',
+      }),
+    },
+    Map: {
+      screen: MapScreen,
+      navigationOptions: () => ({
+        title: 'Map',
+      }),
+    },
   },
   {
-    initialRootName: "Home"
-  }
+    initialRootName: 'Home',
+  },
 );
 // const AppNavigator = createStackNavigator({
 //   Home: {
