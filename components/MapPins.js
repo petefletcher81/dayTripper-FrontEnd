64189e4550b9cd075decaaf6e0be3b8e3a335ddd
RachGriff <rlgriffiths@hotmail.co.uk>
2018-11-30 15:42:03 +0000

    callout working now needs button

--- components/MapPins.js
+++ components/MapPins.js
@@ -1,20 +1,48 @@
-import React from 'react';
-import { MapView, Marker, Callout } from 'expo';
-import { View, Text } from 'react-native';
+import React from "react";
+import { MapView } from "expo";
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
+// import Callout from "react-native-maps";
 
 export default function MapPins(props) {
   return props.attractions.map(attraction => {
-    console.log('pin');
+    console.log(attraction.name);
     return (
       <MapView.Marker
         key={attraction.name}
         coordinate={{
           latitude: attraction.coordinates.latitude,
-          longitude: attraction.coordinates.longitude,
+          longitude: attraction.coordinates.longitude
         }}
-        title={attraction.name}
-        // description={attraction.intro}
-      />
+      >
+        <MapView.Callout style={Styles.callout}>
+          <View>
+            <ScrollView>
+              <Text style={Styles.calloutHeader}>{attraction.name}</Text>
+              <Image
+                style={Styles.placeImage}
+                source={{ uri: attraction.images[0].image }}
+              />
+
+              <Text>{attraction.intro}</Text>
+            </ScrollView>
+          </View>
+        </MapView.Callout>
+      </MapView.Marker>
     );
   });
 }
+
+const Styles = StyleSheet.create({
+  callout: {
+    width: 120,
+    height: 200
+  },
+  placeImage: {
+    width: 100,
+    height: 100
+  },
+  calloutHeader: {
+    fontSize: 16,
+    fontWeight: "bold"
+  }
+});
