66e876574a4776aaeb50b275e0ed2e725bbc37ff
Pete Fletcher <petefletcher@mbp0012-2.local>
2018-12-02 14:24:26 +0000

    userLocation test

--- components/Map.js
+++ components/Map.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { MapView } from 'expo';
+import React from "react";
+import { MapView } from "expo";
 import {
   Platform,
   Button,
   View,
   ActivityIndicator,
   FlatList,
-  Text,
-} from 'react-native';
-import Polyline from '@mapbox/polyline';
-import { Constants, Location, Permissions } from 'expo';
-import GOOGLEAPI from '../config.js';
-import MapPins from './MapPins.js';
+  Text
+} from "react-native";
+import Polyline from "@mapbox/polyline";
+import { Constants, Location, Permissions } from "expo";
+import GOOGLEAPI from "../config.js";
+import MapPins from "./MapPins.js";
 
 export default class MapScreen extends React.Component {
   state = {
@@ -19,14 +19,16 @@ export default class MapScreen extends React.Component {
     longitude: null,
     error: null,
     coordsArray: [],
-    isLoading: true,
+    isLoading: true
   };
 
+  let;
+
   componentWillMount() {
-    if (Platform.OS === 'android' && !Constants.isDevice) {
+    if (Platform.OS === "android" && !Constants.isDevice) {
       this.setState({
         error:
-          'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+          "Oops, this will not work on Sketch in an Android emulator. Try it on your device!"
       });
     } else {
       this._getLocationAsync();
@@ -35,18 +37,26 @@ export default class MapScreen extends React.Component {
 
   _getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
+    if (status !== "granted") {
       this.setState({
-        error: 'Permission to access location was denied',
+        error: "Permission to access location was denied"
       });
     } else {
       let location = await Location.getCurrentPositionAsync({});
       this.setState({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
-        isLoading: false,
+        isLoading: false
       });
     }
+  };
+
+  _beginWatchingLocation = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    this.newLocation = await Location.watchPositionAsync(
+      { distanceInterval: 10 },
+      this._getLocationAsync
+    );
   };
 
   mappingLocations = () => {
@@ -58,7 +68,6 @@ export default class MapScreen extends React.Component {
 
     Promise.all(
       destinationArray.map(destination => {
-        
         return this.getDirections(
           `${this.state.latitude}, ${this.state.longitude}`,
           `${destLocation} ${destination}`
@@ -76,24 +85,24 @@ export default class MapScreen extends React.Component {
       fetch(
         `https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}&mode=walking&key=${
           GOOGLEAPI.GOOGLEDIR
-        }`,
+        }`
       )
         .then(response => response.json())
         //decodes the response
         .then(responseJson => {
           let points = Polyline.decode(
-            responseJson.routes[0].overview_polyline.points,
+            responseJson.routes[0].overview_polyline.points
           );
           let coords = points.map((point, index) => {
             return {
               latitude: point[0],
-              longitude: point[1],
+              longitude: point[1]
             };
           });
 
           const newCoordsArray = [...this.state.coordsArray, coords];
           this.setState({
-            coordsArray: newCoordsArray,
+            coordsArray: newCoordsArray
           });
 
           return newCoordsArray;
@@ -104,12 +113,14 @@ export default class MapScreen extends React.Component {
     );
   }
 
+  cameraViewHandler = () => {};
+
   render() {
     const initialLocation = {
       latitude: this.state.latitude,
       longitude: this.state.longitude,
       latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
+      longitudeDelta: 0.0421
     };
 
     if (this.state.isLoading) {
@@ -121,7 +132,14 @@ export default class MapScreen extends React.Component {
     }
 
     return (
-      <MapView style={{ flex: 1 }} initialRegion={initialLocation}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={initialLocation}
+        showsUserLocation={true}
+        followsUserLocation={true}
+        showsCompass={true}
+        onPress={this.cameraViewHandler}
+      >
         <MapPins
           attractions={this.props.navigation.state.params.randomAttractions}
         />
